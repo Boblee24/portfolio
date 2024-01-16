e1bc717ac401d9831dd5a14f1b111c4ba982a6cb
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-16 10:08:28 +0100

    first commit today

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Skills from './pages/Skills';
+import About from './pages/About';
+// import Projects from './pages/Projects';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Footer from './pages/Footer';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Name is aborisade ayomiposi emmanuel</h1>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="skills" element={<Skills/>} />
+        <Route path='about' element={<About/>}/>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='projects' element={<Projects/>}/>
+        <Route path='*' element={<h1> PAGE NOT FOUND</h1>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Logo</h1>
+        <div>
+            <ul>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/projects">Projects</NavLink></li>
+                <li><NavLink to="/skills">Skills</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
