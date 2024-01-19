a1d2eedb4b1804ec8aebadba30b54d7d59fa3b74
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-19 20:02:35 +0100

    commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ import { MdOutlineMail } from "react-icons/md";
 const Contact = () => {
   return (
     <motion.div
-      className="h-[100dvh] flex justify-center items-center z-0 bg-orange-300"
+      className="h-[100dvh] flex justify-center items-center z-0"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
@@ -13,8 +13,8 @@ const Contact = () => {
       <div className="flex justify-between w-full items-center p-[5%]">
         <h1>Contact </h1>
         <div className="">
-          <div className="w-[300px] z-[11111111] relative h-[150px] bg-[#5c5c5c]">
-            <div className=" hello absolute w-full h-full left-2 bg-slate-300 z-[5] top-3"></div>
+          <div className=" parent ">
+            <div className=" hello"></div>
             <div>
               <MdOutlineMail />
               <h3>Email</h3>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
-// import myImage from "../assts/ayomiposi.jpg" 
-// import myImage2 from "../assts/ayomi.jpeg" 
-import myImage3 from "../assts/ayomi1.jpeg" 
+// import myImage from "../assts/ayomiposi.jpg"
+// import myImage2 from "../assts/ayomi.jpeg"
+import myImage3 from "../assts/ayomi1.jpeg";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <motion.div
       className=" h-[100dvh]  flex justify-center items-center"
@@ -13,19 +13,26 @@ const Home = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-        <div className="w-full h-full home">
-
+      <div className="w-full h-full home"></div>
+      <div className="flex absolute justify-between w-full px-[4%] items-center">
+        <div className="text-center">
+          <h2 className="text-[2rem]">HELLO, I'm</h2>
+          <h1 className="text-[3rem] font-bold">Aborisade Ayomiposi</h1>
+          <h2 className="text-[2rem]">A Frontend Web Developer</h2>
+          <button
+            onClick={() => navigate("/contact")}
+            className="px-4 py-3 bg-[black] rounded-lg text-white tracking-widest"
+          >
+            Contact me
+          </button>
         </div>
-        <div className="flex absolute justify-between w-full px-[4%] items-center">
-          <div className="text-center">
-            <h2 className="text-[2rem]">HELLO, I'm</h2>
-            <h1 className="text-[3rem] font-bold">Aborisade Ayomiposi</h1>
-            <h2 className="text-[2rem]">A Frontend Web Developer</h2>
-            <button onClick={() => navigate("/contact")} className="px-4 py-3 bg-[black] rounded-lg text-white tracking-widest">Contact me</button>
-          </div>
-          <div className="max-w-[500px] h-[500px]">
-            <img src={myImage3} className="w-full h-full border-2 border-zinc-200" alt="ayomiposi"/>
-          </div>
+        <div className="max-w-[500px] h-[500px]">
+          <img
+            src={myImage3}
+            className="w-full h-full border-2 border-zinc-200"
+            alt="ayomiposi"
+          />
+        </div>
       </div>
     </motion.div>
   );
