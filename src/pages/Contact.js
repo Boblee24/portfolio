6d657affc82d3743c59b01e6343b84884c1d50e8
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-19 19:37:33 +0100

    enter

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,15 +5,15 @@ import { MdOutlineMail } from "react-icons/md";
 const Contact = () => {
   return (
     <motion.div
-      className="h-[100dvh] flex justify-center items-center"
+      className="h-[100dvh] flex justify-center items-center z-0 bg-orange-300"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="flex justify-between w-full items-center">
+      <div className="flex justify-between w-full items-center p-[5%]">
         <h1>Contact </h1>
-        <div>
-          <div>
+        <div className="relative">
+          <div className="w-[300px] z-10 relative h-[150px] bg-[#5c5c5c] before:absolute before:w-full before:h-full before:inset-0 before:left-2 before:bg-slate-300 before:z-10 before:top-3">
             <div>
               <MdOutlineMail />
               <h3>Email</h3>
