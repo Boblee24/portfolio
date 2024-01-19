19d0010131d44d6c2c1d0a50a5c5e471ab2bd2d9
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-19 19:42:10 +0100

    enter

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
       <div className="flex justify-between w-full items-center p-[5%]">
         <h1>Contact </h1>
         <div className="relative">
-          <div className="w-[300px] z-10 relative h-[150px] bg-[#5c5c5c] before:absolute before:w-full before:h-full before:inset-0 before:left-2 before:bg-slate-300 before:z-10 before:top-3">
+          <div className="w-[300px] z-[1] relative h-[150px] bg-[#5c5c5c] before:absolute before:w-full before:h-full md:before:content-['soft'] before:left-2 before:bg-slate-300 before:z-[-5] before:top-3">
             <div>
               <MdOutlineMail />
               <h3>Email</h3>
