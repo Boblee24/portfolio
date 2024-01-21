41fc17e694164524c1766d04b7e86e811893d0d7
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-21 01:37:55 +0100

    soft

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -38,12 +38,12 @@ const Contact = () => {
                 key={contact.id}
               >
                 <div className=" hello group-hover:bg-[#808080] duration-700  ease-in-out transition border-2 border-black "></div>
-                <div className="z-1 border-b-2 border-l-2 p-5 border-black h-full gap-2 bg-[#b0b0b0] flex flex-col ">
-                  <div className="flex">
+                <div className="z-1 border-b-2 border-l-2 p-4 border-black h-full gap-2 bg-[#b0b0b0] flex flex-col ">
+                  <div className="flex gap-2">
                     <div className="">{getIcon}</div>
-                    <h3 className="text-[1.7rem]">{contact.name}</h3>
+                    <h3 className="text-[1.4rem]">{contact.name}</h3>
                   </div>
-                  <h4>{contact.username}</h4>
+                  <h4 className="italic">{contact.username}</h4>
                   <a href={contact.url} className="self-start py-2 px-4 bg-black text-[#808080]">Visit &rarr;</a>
                 </div>
               </div>
