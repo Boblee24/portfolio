4a3eee61feda7905404a5d2f9dcb9f3086275a8d
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-21 01:34:04 +0100

    soon

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,18 +15,18 @@ const Contact = () => {
     >
       <div className="flex justify-between w-full items-center p-[5%]">
         <h1>Contact </h1>
-        <div className=" grid-cols-2 grid gap-[3rem]">
+        <div className=" grid-cols-2 grid gap-[6rem]">
           {Contacts.map((contact) => {
             const iconComponent = (thatIcon) => {
               switch (thatIcon) {
                 case "MdOutlineMail":
-                  return <MdOutlineMail />;
+                  return <MdOutlineMail size={30} />;
                 case "LinkedIn":
-                  return <FaLinkedin />;
+                  return <FaLinkedin size={30} />;
                 case "Twitter":
-                  return <FaTwitter />;
+                  return <FaTwitter size={30} />;
                 case "Github":
-                  return <FaGithub />;
+                  return <FaGithub size={30}  />;
                 default:
                   return null;
               }
@@ -34,17 +34,17 @@ const Contact = () => {
             const getIcon = iconComponent(contact.icon);
             return (
               <div
-                className=" parent group hover:scale-110 duration-700"
+                className=" parent group hover:scale-110 duration-700 "
                 key={contact.id}
               >
-                <div className=" hello group-hover:bg-white duration-700  ease-in-out transition omoo "></div>
-                <div className="z-1 bg-slate-300 h-full">
-                  <div className="flex this">
-                    {getIcon}
-                    <h3>{contact.name}</h3>
+                <div className=" hello group-hover:bg-[#808080] duration-700  ease-in-out transition border-2 border-black "></div>
+                <div className="z-1 border-b-2 border-l-2 p-5 border-black h-full gap-2 bg-[#b0b0b0] flex flex-col ">
+                  <div className="flex">
+                    <div className="">{getIcon}</div>
+                    <h3 className="text-[1.7rem]">{contact.name}</h3>
                   </div>
                   <h4>{contact.username}</h4>
-                  <button>Visit the link broooo</button>
+                  <a href={contact.url} className="self-start py-2 px-4 bg-black text-[#808080]">Visit &rarr;</a>
                 </div>
               </div>
             );
