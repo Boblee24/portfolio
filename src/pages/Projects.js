0e8920cc405e98fd2ba8d7b0caa04e92c8ad937b
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-24 15:50:35 +0100

    first comitof the day

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,46 @@
 import React from "react";
 
 const Projects = (props) => {
+  const imageFunc = (string) => {
+    const image = require(`../assts/${string}`);
+    return image;
+  };
+  // const handleMouseEnter = (e) => {
+  //   const image = e.target;
+  //   const scrollSpeed = 5; // Adjust the scroll speed as needed
+  //   image.style.transition = `transform ${scrollSpeed}s linear`;
+  //   image.style.transform = "translateY(-100%)";
+  // };
+
+  // const handleMouseLeave = (e) => {
+  //   const image = e.target;
+  //   image.style.transition = "none";
+  //   image.style.transform = "translateY(0)";
+  // };
+  // onMouseEnter={handleMouseEnter}
+  //           onMouseLeave={handleMouseLeave}
   const MyProject = () => {
     return (
       <div className="">
         {props.myProjects.map((myProject) => (
-          <div>
-            {/* <img src={myProject.img} alt={myProject.name} /> */}
-            {myProject.id}
+          <div key={myProject.id} className="w-[300px]">
+            <div className="overflow-hidden h-[300px] w-[300px] m-2">
+              <img
+                className=" duration-[2000ms] h-full w-full object-top hover:object-bottom object-cover rounded-lg"
+                src={imageFunc(myProject.img)}
+                alt={myProject.name}
+
+              />
+            </div>
+            <h2>{myProject.name}</h2>
+            <h3>{myProject.description}</h3>
           </div>
         ))}
       </div>
     );
   };
   return (
-    <div>
+    <div className="pt-[5rem]">
       <h1>Projects</h1>
       <div>{MyProject()}</div>
     </div>
