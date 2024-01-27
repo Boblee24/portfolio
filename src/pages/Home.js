6b6706e12619dd684f1306bea7abc21b1bd59626
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-28 00:45:28 +0100

    modifications

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
 // import myImage from "../assts/ayomiposi.jpg"
-// import myImage2 from "../assts/ayomi.jpeg"
-import myImage3 from "../assts/ayomi1.jpeg";
+import myImage2 from "../assts/ayomi.png"
+// import myImage2 from "../assts/ayomi3.png"
+// import myImage3 from "../assts/ayomi1.jpeg";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
@@ -13,12 +14,12 @@ const Home = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="w-full h-full home hidden"></div>
-      <div className="flex absolute justify-between w-full px-[4%] items-center">
-        <div className="text-center">
-          <h2 className="text-[2rem]">HELLO, I'm</h2>
-          <h1 className="text-[3rem] font-bold">Aborisade Ayomiposi</h1>
-          <h2 className="text-[2rem]">A Frontend Web Developer</h2>
+      {/* <div className="w-full h-full home hidden"></div> */}
+      <div className="flex relative justify-between h-[100vh] w-full  items-center bg-black">
+        <div className="text-center text-white z-10 absolute bottom-0 bg-[#d7d7d785] w-full">
+          <h2 className="text-[1.1rem]">HELLO, I'm</h2>
+          <h1 className="text-[1.5rem] font-bold">Aborisade Ayomiposi</h1>
+          <h2 className="text-[1.2rem]">A Frontend Web Developer</h2>
           <button
             onClick={() => navigate("/contact")}
             className="px-4 py-3 bg-[black] rounded-lg text-white tracking-widest"
@@ -26,10 +27,10 @@ const Home = () => {
             Contact me
           </button>
         </div>
-        <div className="max-w-[500px] h-[500px] hidden">
+        <div className=" w-[150%] left-[-4rem] top-[-4rem] absolute">
           <img
-            src={myImage3}
-            className="w-full h-full border-2 border-zinc-200"
+            src={myImage2}
+            className="w-full h-full "
             alt="ayomiposi"
           />
         </div>
