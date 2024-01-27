203defbe34593684cbd45b29190ced966fd5b3b6
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-27 15:07:04 +0100

    mod

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,17 +9,19 @@ const Projects = (props) => {
     return image;
   };
 
-  const MyProjectItem = ({myProject}) => {
+  const MyProjectItem = ({ myProject }) => {
     const targetRef = useRef();
     const [intersected, setIntersected] = useState(null);
     useEffect(() => {
-      const observer = new IntersectionObserver((entries) => {
-        const entry = entries[0];
-        setIntersected(entry.isIntersecting);
-      },
-      {
-        threshold: 0.5, // Set your desired threshold value (here, 50% visibility)
-      });
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const entry = entries[0];
+          setIntersected(entry.isIntersecting);
+        },
+        {
+          threshold: 0.5, // Set your desired threshold value (here, 50% visibility)
+        }
+      );
       observer.observe(targetRef.current);
       return () => {
         observer.disconnect(); // Cleanup observer on unmount
@@ -27,31 +29,35 @@ const Projects = (props) => {
     }, []);
     console.log(intersected);
     return (
-      <div key={myProject.id} ref={targetRef} className="relative">
+      <div key={myProject.id} ref={targetRef} className="">
         <div
           ref={targetRef}
-          className={`flex flex-col m-2 gap-2 active:bg-[red] `}
+          className={`flex flex-col m-2 gap-2 active:bg-[red]  ] `}
         >
-          <div className="overflow-hidden h-[300px] ">
+          <div className="overflow-hidden h-[300px] z-[-1] relative ">
             <img
-              className=" duration-[2000ms] h-full w-full object-top hover:object-bottom  object-cover rounded-lg"
+              className=" duration-[2000ms] h-full w-full z-20 object-top hover:object-bottom  object-cover rounded-lg"
               src={imageFunc(myProject.img)}
               alt={myProject.name}
             />
-          </div>
-          <div className={`absolute bg-[#d7d7d7ab] duration-1000 h-full ${intersected ? "" : "notvisible"}`}>
-            <h2 className="text-[2.5rem] font-bold ">{myProject.name}</h2>
-            <h3 className="text-[1.1rem] font-semibold">
-              {myProject.description}
-            </h3>
-            <div className="flex gap-7">
-              <Link to="" className="text-[1.5rem]">
-                Demo
-              </Link>
-              <Link to="" className="flex items-center">
-                <FaGithub size={25} />
-                <h2 className="text-[1.5rem]">Github</h2>
-              </Link>
+            <div
+              className={`absolute bg-[#000000ab] rounded-lg p-2 duration-1000 z-60 h-full w-full top-0 ${
+                intersected ? "visible" : "notvisible"
+              }`}
+            >
+              <h2 className="text-[2.5rem] font-bold ">{myProject.name}</h2>
+              <h3 className="text-[1.1rem] font-semibold">
+                {myProject.description}
+              </h3>
+              <div className="flex gap-7 my-6 text-white">
+                <Link to="" className="text-[1.5rem]">
+                  Demo
+                </Link>
+                <Link to="" className="flex items-center gap-2">
+                  <FaGithub size={25} />
+                  <h2 className="text-[1.5rem]">Github</h2>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
