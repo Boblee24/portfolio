7f3c56c6338bc3761f09843759f736f904ef2697
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-28 01:07:39 +0100

    mod

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 // import myImage from "../assts/ayomiposi.jpg"
-import myImage2 from "../assts/ayomi.png"
+import myImage2 from "../assts/ayomi.png";
 // import myImage2 from "../assts/ayomi3.png"
 // import myImage3 from "../assts/ayomi1.jpeg";
 import { useNavigate } from "react-router-dom";
@@ -16,23 +16,25 @@ const Home = () => {
     >
       {/* <div className="w-full h-full home hidden"></div> */}
       <div className="flex relative justify-between h-[100vh] w-full  items-center bg-black">
-        <div className="text-center text-white z-10 absolute bottom-0 bg-[#d7d7d785] w-full">
-          <h2 className="text-[1.1rem]">HELLO, I'm</h2>
-          <h1 className="text-[1.5rem] font-bold">Aborisade Ayomiposi</h1>
-          <h2 className="text-[1.2rem]">A Frontend Web Developer</h2>
-          <button
-            onClick={() => navigate("/contact")}
-            className="px-4 py-3 bg-[black] rounded-lg text-white tracking-widest"
-          >
-            Contact me
-          </button>
+        <div className="text-left home h-[50%]  text-white z-10 absolute bottom-0 bg-[#525252b8] w-full">
+          <div className="absolute bottom-0 p-2 flex justify-between w-full">
+            <div>
+              <h2 className="text-[1.1rem]">HELLO, I'm</h2>
+              <h1 className="text-[1.5rem] font-bold">Aborisade Ayomiposi</h1>
+              <h2 className="text-[1.2rem]">A Frontend Web Developer</h2>
+            </div>
+            <div>
+              <button
+                onClick={() => navigate("/contact")}
+                className="px-4 py-3 bg-[black] rounded-lg text-white tracking-widest sidebar"
+              >
+                Contact me
+              </button>
+            </div>
+          </div>
         </div>
         <div className=" w-[150%] left-[-4rem] top-[-4rem] absolute">
-          <img
-            src={myImage2}
-            className="w-full h-full "
-            alt="ayomiposi"
-          />
+          <img src={myImage2} className="w-full h-full " alt="ayomiposi" />
         </div>
       </div>
     </motion.div>
