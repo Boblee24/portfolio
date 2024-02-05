5a68de20c85cae1a7c6a05d306a096239170aa16
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-02-05 22:42:20 +0100

    commit

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,7 +4,6 @@ import { AppContext } from "../App";
 import seperator from "../assts/separator.svg";
 
 const Skills = () => {
-
   const { Languages, slide } = useContext(AppContext);
   const imageFunc = (string) => {
     const image = require(`../assts/${string}`);
@@ -12,13 +11,20 @@ const Skills = () => {
   };
   const skills = () => {
     return (
-      <div className={`grid grid-cols-3 w-full gap-[1.5rem] ${slide ? 'mt-[6rem]' : "mt-[1rem]"} p-2`}>
+      <motion.div
+        initial={{ opacity: 0, y : '3rem'}}
+        whileInView={{ opacity: 1, y : 0 }}
+        transition = {{delay: 0.2, duration: .5 }}
+        className={`grid grid-cols-3 w-full gap-[1.5rem] ${
+          slide ? "mt-[6rem]" : "mt-[1rem]"
+        } p-2`}
+      >
         {Languages.map((Language) => (
           <div className="hover:scale-110 duration-300 p-2" key={Language.id}>
             <img src={imageFunc(Language.image)} alt={Language.name} />
           </div>
         ))}
-      </div>
+      </motion.div>
     );
   };
   return (
@@ -30,8 +36,16 @@ const Skills = () => {
       id="skills"
     >
       <div className="flex flex-col w-full h-full items-center justify-between">
-        <div className={`bg-[#D7D7D7] z-10 top-0  ${slide ? "fixed" : "block"} left-0 w-full pb-3 m-2`}>
-          <h1 className={`text-[2.5rem] font-semibold ${slide ? "mt-[3rem]" : "mt-0"} self-start  `}>
+        <div
+          className={`bg-[#D7D7D7] z-10 top-0  ${
+            slide ? "fixed" : "block"
+          } left-0 w-full pb-3 m-2`}
+        >
+          <h1
+            className={`text-[2.5rem] font-semibold ${
+              slide ? "mt-[3rem]" : "mt-0"
+            } self-start  `}
+          >
             My Skills
           </h1>
           <img src={seperator} alt="seperator" />
