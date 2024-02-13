660b3d7640b1401e65c27e375739a8daf145bbbe
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-02-13 23:59:41 +0100

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,13 +16,13 @@ const Home = () => {
       id="/"
     >
       {/* <div className="w-full h-full home hidden"></div> */}
-      <div className="flex relative justify-between h-[100vh] xxdm:h-auto w-full xxdm:w-auto items-center">
-        <div className="text-left home h-[50%]  text-white z-10 absolute xxdm:static bottom-0 bg-[#525252d2] xxdm:bg-inherit w-full">
+      <div className="flex relative justify-between h-[100vh] xxdm:h-auto w-full xxdm:w-full items-center">
+        <div className="text-center home h-[50%] text-white z-10 absolute xxdm:static bottom-0 bg-[#525252d2] xxdm:bg-inherit w-full">
           <div className="absolute xxdm:static bottom-0 p-2 flex xxdm:flex-col justify-between w-full">
             <div className="text-[white]">
               <h2 className="text-[1.1rem] xxdm:text-[2rem]">HELLO, I'm</h2>
-              <h1 className="text-[1.5rem] font-bold xxdm:text-[3rem]">
-                Aborisade Ayomiposi
+              <h1 className="text-[1.5rem] font-bold xxdm:text-[2.3rem]">
+                ABORISADE AYOMIPOSI
               </h1>
               <h2 className="text-[1.2rem] xxdm:text-[2.5rem]">
                 A Frontend Web Developer
@@ -38,10 +38,10 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className=" w-[150%] xx:w-[120%] left-[-3rem] xx:top-[-6rem] xx:left-[-1rem] top-[-4rem] xii:w-full xii:left-[6rem] dm:top-[-8rem] absolute xdm:top-[-30%] xdm:left-[20%] xxdm:static ">
+        <div className=" w-[150%] xx:w-[120%] left-[-3rem] xx:top-[-6rem] xx:left-[-1rem] top-[-4rem] xii:w-auto xii:left-[6rem] dm:top-[-8rem] absolute xdm:top-[-30%] xdm:left-[20%] xxdm:static ">
           <div className="xxdm:h-[400px] xxdm:w-[400px] xxdm:relative xxdm:border-2 xxdm:border-[#d7d7d7] z-[8]">
-            <div className="hello group-hover:bg-[#808080] duration-700  ease-in-out transition xxdm:border-2 xxdm:border-white xxdm:z-[-10]"></div>
-            <div className="w-full h-full overflow-hidden z-1 bg-black">
+            <div className="hello group-hover:bg-[#808080] duration-700  ease-in-out transition top-[2rem] xxdm:left-[2rem] xxdm:border-2  xxdm:border-white xxdm:z-[-10]"></div>
+            <div className="w-full h-full overflow-hidden z-1 bg-[#1e1e1e]">
               <img
                 src={myImage2}
                 className="w-full h-full xxdm:h-auto opacity-[1] "
