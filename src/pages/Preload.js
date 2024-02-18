7577a1fc607ff356dacf4a097724982205647117
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-02-18 17:07:03 +0100

    animation

--- src/pages/Preload.js
+++ src/pages/Preload.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useEffect, useState } from 'react'
-import '../PreloadScreen.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "../PreloadScreen.css";
 
 const Preload = ({ onLoadingComplete }) => {
   const [loading, setLoading] = useState(true);
@@ -9,24 +9,22 @@ const Preload = ({ onLoadingComplete }) => {
     const timer = setTimeout(() => {
       setLoading(false);
       onLoadingComplete(); // Trigger the callback when loading is complete
-    }, 3000); // Adjust the time (in milliseconds) as needed
+    }, 4000); // Adjust the time (in milliseconds) as needed
 
     return () => clearTimeout(timer);
   }, [onLoadingComplete]);
 
   return (
-    <div className={`preload-container ${loading ? 'loading' : 'loaded'}`}>
+    <div className={`preload-container ${loading ? "loading" : "loaded"}`}>
       <div className="preload-overlay"></div>
       <div className="preload-content">
-        <h1>Welcome to My Portfolio</h1>
-        {/* Add other content or loading animations here */}
+        <div class="escape">Escape</div>
+        <div class="experience">
+          <span>into amazing experiences</span>
+        </div>
       </div>
-      
     </div>
   );
 };
 
-
-
-
-export default Preload
+export default Preload;
